22792af9ea82977e0a63c29128ff60560b0a5fb4
Mayna-B <me2be2014@gmail.com>
2023-05-11 20:21:00 -0400

    added validation

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,37 +2,36 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 // GET all the bread
-router.get('/', (req, res) => {
-    res.render('index', {
-        breads: Bread
+router.get('/', async (req, res) => {
+      const bread = await Bread.find()
+      res.render('index', {
+        breads: bread
       })
 })
-
 
 router.get('/new', (req, res) => {
       res.render('new')
 })
 
-router.get('/:index/edit', (req, res) => {
-      const { index } = req.params
+router.get('/:id/edit', async (req, res) => {
+      const { id } = req.params
+      const bread = await Bread.findById(id)
       res.render('edit', {
-          bread: Bread[index],
-          index: index
+          bread
         })
   })
 
 //GET bread a specific bread by index
-router.get('/:index', (req, res) => {
-    const { index } = req.params
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const bread = await Bread.findById(id)
     res.render('show', {
-        bread: Bread[index],
-        index
+        bread
     })
 })
 
-router.post('/', (req, res) => {
-      if (!req.body.image ) 
-            req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+router.post('/', async (req, res) => {
+      if (!req.body.image ) req.body.image = undefined
 
       if (req.body.hasGluten === 'on') {
             req.body.hasGluten = true
@@ -40,29 +39,58 @@ router.post('/', (req, res) => {
             req.body.hasGluten = false
       }
 
-      Bread.push(req.body)
+      await Bread.create(req.body)
       res.status(303).redirect('/breads')
 })
 
-router.delete('/:index', (req, res) => {
-      const { index } = req.params
-      Bread.splice(index, 1)
+router.delete('/:id', async (req, res) => {
+      const { id } = req.params
+      await Bread.findByIdAndDelete(id)
       res.status(303).redirect('/breads')
 })
 
-router.put('/:index', (req, res) => {
-      const { index } = req.params
-      if (!req.body.image ) 
-            req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+router.put('/:id', async (req, res) => {
+      const { id } = req.params
 
+      if (!req.body.image) req.body.image = undefined
       if (req.body.hasGluten === 'on') {
             req.body.hasGluten = true
       } else {
             req.body.hasGluten = false
       }
 
-      Bread[index] = req.body
-      res.status(303).redirect(`/breads/${index}`)
+      await Bread.findByIdAndUpdate(id, req.body)
+      res.status(303).redirect(`/breads/${id}`)
 })
+
+// Database seed
+router.get('/data/seed', async (req, res) => {
+      const data = [
+          {
+            name: 'Rye',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          },
+          {
+            name: 'French',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+          },
+          {
+            name: 'Gluten Free',
+            hasGluten: false,
+            image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          },
+          {
+            name: 'Pumpernickel',
+            hasGluten: true,
+            image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+          }
+      ]
+  
+      await Bread.insertMany(data)
+      res.status(303).redirect('/breads');
+  })
+
 
 module.exports = router
--- views/edit.jsx
+++ views/edit.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Edit({bread, index}) {
+function Edit({ bread }) {
     return (
       <Default>
         <h2>Edit a bread</h2>
-        <form action={`/breads/${index}?_method=PUT`} method='POST'>
+        <form action={`/breads/${bread.id}?_method=PUT`} method='POST'>
           <label htmlFor="name">Name</label>
           <input
             type="text"
@@ -22,6 +22,15 @@ function Edit({bread, index}) {
             defaultValue={bread.image}
           />
           <label htmlFor="hasGluten">Has Gluten?</label>
+          <label htmlFor="baker">Baker</label>
+          <select name="baker" id="baker" defaultValue={bread.baker}>
+            <option value="Rachel">Rachel</option>
+            <option value="Monica">Monica</option>
+            <option value="Joey">Joey</option>
+            <option value="Chandler">Chandler</option>
+            <option value="Ross">Ross</option>
+            <option value="Phoebe">Phoebe</option>
+          </select>
           <input
             type="checkbox"
             name="hasGluten"
